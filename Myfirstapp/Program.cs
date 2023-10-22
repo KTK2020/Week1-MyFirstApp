069baf5c2456e89cd83e6d93e0efff769e5a5226
KTK20202 <jalliksa@tktk.ee>
2023-10-22 18:51:58 +0300

    add myfirstapp code

---
+++ Myfirstapp/Program.cs
@@ -0,0 +1,17 @@
+﻿// This is a comment to my code
+
+//rakendus küsib kasutajalt sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name"); //outpu
+//string - sõne
+string userName = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //Output
+
+//string interpolation
+Console.WriteLine($"Hello, {userName} !"); //Output
+
+firstName = "Harry";
+lastname = "Potter";
+Console.WriteLine($"Hello, {firstName} lastName!")
